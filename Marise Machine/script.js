7763179edb3ccfa8667405840c810ba1ee685173
Th-Sanchez <thiago.peluci@sptech.school>
2024-09-23 20:45:22 -0300

    Alteração de muitas coisas

--- Marise Machine/script.js
+++ Marise Machine/script.js
@@ -93,3 +93,87 @@ function subtracaoDecimal() {
         div_resultado_subtracao.innerHTML = `A subtração entra os número é de ${resultado}`
     }
 }
+
+function multiplicacaoDecimal() {
+    const numero1 = Number(input_numero5.value)
+    const numero2 = Number(input_numero6.value)
+    var resultado
+
+    resultado = numero1 * numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_multiplicacao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_multiplicacao.innerHTML = `A multiplicação entra os número é de ${resultado}`
+    }
+}
+
+function divisaoDecimal() {
+    const numero1 = Number(input_numero7.value)
+    const numero2 = Number(input_numero8.value)
+    var resultado
+
+    resultado = numero1 / numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_divisao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_divisao.innerHTML = `A divisão entra os número é de ${resultado}`
+    }
+}
+
+function somaBinario() {
+    const numero1 = parseInt(input_numero1.value).toString(10)
+    const numero2 = parseInt(input_numero2.value, 2).toString(10)
+    var resultado
+
+    resultado = numero1 + numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_soma.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_soma.innerHTML = `A soma entra os número é de ${resultado.toString(2)}`
+    }
+}
+
+function subtracaoBinario() {
+    const numero1 = Number(input_numero3.value)
+    const numero2 = Number(input_numero4.value)
+    var resultado
+
+    resultado = numero1 - numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_subtracao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_subtracao.innerHTML = `A subtração entra os número é de ${resultado}`
+    }
+}
+
+function multiplicacaoBinario() {
+    const numero1 = Number(input_numero5.value)
+    const numero2 = Number(input_numero6.value)
+    var resultado
+
+    resultado = numero1 * numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_multiplicacao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_multiplicacao.innerHTML = `A multiplicação entra os número é de ${resultado}`
+    }
+}
+
+function divisaoBinario() {
+    const numero1 = Number(input_numero7.value)
+    const numero2 = Number(input_numero8.value)
+    var resultado
+
+    resultado = numero1 / numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_divisao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_divisao.innerHTML = `A divisão entra os número é de ${resultado}`
+    }
+}
