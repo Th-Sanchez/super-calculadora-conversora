232c64a42e96113ebc4ea91539a68917617eb594
Th-Sanchez <thiago.peluci@sptech.school>
2024-09-22 23:38:20 -0300

    Implementação de recursos adicionais

--- Marise Machine/script.js
+++ Marise Machine/script.js
@@ -1,12 +1,18 @@
 function converterDecimal() {
     var decimal = Number(input_numero.value);
+    let binario = decimal.toString(2);
     let octal = decimal.toString(8);
     let hexadecimal = decimal.toString(16);
-    let binario = decimal.toString(2);
 
-    resultado_binario.innerHTML = ` ${binario}`;
-    resultado_octal.innerHTML = ` ${octal}`;
-    resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    if (isNaN(binario) || isNaN(octal) || isNaN(hexadecimal)) {
+        resultado_binario.innerHTML = `Valor inválido`
+        resultado_octal.innerHTML = `Valor inválido`
+        resultado_hexadecimal.innerHTML = `Valor inválido`    
+    } else {
+        resultado_binario.innerHTML = ` ${binario}`;
+        resultado_octal.innerHTML = ` ${octal}`;
+        resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    }
 }
 
 function converterBinario() {
@@ -15,9 +21,15 @@ function converterBinario() {
     let octal = parseInt(binario, 2).toString(8);
     let hexadecimal = parseInt(binario, 2).toString(16);
 
-    resultado_decimal.innerHTML = ` ${decimal}`;
-    resultado_octal.innerHTML = ` ${octal}`;
-    resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    if (isNaN(decimal) || isNaN(octal) || isNaN(hexadecimal)) {
+        resultado_decimal.innerHTML = `Valor inválido`
+        resultado_octal.innerHTML = `Valor inválido`   
+        resultado_hexadecimal.innerHTML = `Valor inválido` 
+    } else {
+        resultado_decimal.innerHTML = ` ${decimal}`;
+        resultado_octal.innerHTML = ` ${octal}`;
+        resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    }
 }
 
 function converterOctal() {
@@ -26,9 +38,15 @@ function converterOctal() {
     let binario = parseInt(octal, 8).toString(2);
     let hexadecimal = parseInt(octal, 8).toString(16);
 
-    resultado_decimal.innerHTML = ` ${decimal}`;
-    resultado_binario.innerHTML = ` ${binario}`;
-    resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    if (isNaN(decimal) || isNaN(binario) || isNaN(hexadecimal)) {
+        resultado_decimal.innerHTML = `Valor inválido`
+        resultado_binario.innerHTML = `Valor inválido`
+        resultado_hexadecimal.innerHTML = `Valor inválido`    
+    } else {
+        resultado_decimal.innerHTML = ` ${decimal}`;
+        resultado_binario.innerHTML = ` ${binario}`;
+        resultado_hexadecimal.innerHTML = ` ${hexadecimal}`;
+    }
 }
 
 function converterHexa() {
@@ -45,5 +63,33 @@ function converterHexa() {
         resultado_decimal.innerHTML = ` ${decimal}`;
         resultado_binario.innerHTML = ` ${binario}`;
         resultado_octal.innerHTML = ` ${octal}`;
+    }
+}
+
+function somaDecimal() {
+    const numero1 = Number(input_numero1.value)
+    const numero2 = Number(input_numero2.value)
+    var resultado
+
+    resultado = numero1 + numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_soma.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_soma.innerHTML = `A soma entra os número é de ${resultado}`
+    }
+}
+
+function subtracaoDecimal() {
+    const numero1 = Number(input_numero3.value)
+    const numero2 = Number(input_numero4.value)
+    var resultado
+
+    resultado = numero1 - numero2
+    h3_texto.innerHTML = `Resultado`
+    if (isNaN(numero1) || isNaN(numero2) || isNaN(resultado)){
+        div_resultado_subtracao.innerHTML = `Valores inválidos`
+    } else {
+        div_resultado_subtracao.innerHTML = `A subtração entra os número é de ${resultado}`
     }
 }
